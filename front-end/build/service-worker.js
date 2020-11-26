52f80fb93fa9b7596f4e7889fd2a13eb2a8b3408
Georgii Goncharenko <georg.gon4arenko2012@yandex.ru>
2020-11-26 14:46:19 +0300

    react-mesto-api-full

--- front-end/build/service-worker.js
+++ front-end/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c1dd292765cfcbb0393cb7b965dddcb6.js"
+  "/precache-manifest.3b63836096c4fec713dda1627143f1ab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
